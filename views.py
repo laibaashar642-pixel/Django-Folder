4b9d4681ab305ba74f6b54369892d4df2b80464b
Laiba Ashar <asharmunir642@gmail.com>
2026-03-08 12:21:46 +0500

    Add files via upload

--- views.py
+++ views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import*
+
 # Create your views here.
-""" def index(request):
-    return HttpResponse("Hey! This is i am") """
-def index(request):
-    people={
-"name":"John",
-"age":30,
-"city":"New York",
-"Title":"Django Developer",
-    }
-    
-    return render(request,"index.html",context={'people':people})
-def success(request):
-    return HttpResponse("Hey! This is success page")
+def recepie(request):
+    # so frontend sai backened mai data lanay kai liya hum ye use krty hai
+    if request.method=="Post":
+        data=request.post
+        # for files
+        recepie_image=request.Files.get("recepie_image")
+        recepie_name=data.get("recepie_name")
+        recepie_quantity=data.get("recepie_quantity")
+        Recepie.objects.create(
+            recepie_name=recepie_name,
+            recepie_quantity=recepie_quantity,
+            recepie_image=recepie_image)
+        print(recepie_name)
+        print(recepie_quantity)
+        print(recepie_image)
+    return render(request,"recepie.html")
+#aur agr hamy is data ko save krna hai
